fc6361024068bdb1b8341fa06694ebf820d03f78
Taneli Riihimäki <taneli.riihimaki@liveto.io>
2021-10-03 07:25:56 +0300

    Add font awesome and a  font (Space Grotesk)

--- portalcodivasapp/src/App.js
+++ portalcodivasapp/src/App.js
@@ -3,9 +3,21 @@ import Navigation from "./Components/Navigation";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Footer from "./Components/Footer";
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { 
+  faSearch,
+  faPaperPlane,
+  faBell,
+  faUserAlt
+ } from '@fortawesome/free-solid-svg-icons'
+
+
 import './sass/index.scss';
 
 function App() {
+  library.add([faSearch, faPaperPlane, faBell,faUserAlt])
+  dom.watch()
+
   return (
     <div className="layout">
       <Navigation />
--- portalcodivasapp/src/Components/Header.jsx
+++ portalcodivasapp/src/Components/Header.jsx
@@ -1,8 +1,32 @@
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Header = () => {
+    
     return (
         <header className="header">
-            <div className="search"></div>
-            <div className="user"></div>
+            <div className="search">
+                <InputGroup className="mx-3">
+                    <FormControl
+                    aria-label="Search"
+                    aria-describedby="search-input"
+                    />
+                    <InputGroup.Text id="search-input"><FontAwesomeIcon icon={["fa", "search"]} /></InputGroup.Text>
+                </InputGroup>
+            </div>
+            <div className="user">
+                <div className="notifications">
+                    <FontAwesomeIcon icon={["fa", "bell"]} />
+                    </div>
+                <div className="messages">
+                    <FontAwesomeIcon icon={["fa", "paper-plane"]} />
+                    </div>
+                <div className="profile">
+                    <span className="name">Name</span>
+                    <FontAwesomeIcon icon={["fa", "user-alt"]} />
+                </div>
+            </div>
         </header>
     )
 }
