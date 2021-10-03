f1e5a9c340e41b82c5f5746d887493bd1b4def74
nisha <nishayadav25487062gmail.com>
2021-10-03 12:14:14 +0530

    added footer

---
+++ portalcodivasapp/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+import Footer from "./Footer";
+
+
+function App(){
+    return <div>
+        <Footer />   
+    </div>
+}
+export default App;
