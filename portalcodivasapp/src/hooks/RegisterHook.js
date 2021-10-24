ed3984e39e7a3156904328bfd90b8ee90c20c919
Daviny <daviny@vidal.dev.br>
2021-10-24 18:06:30 -0300

    fix cors

--- portalcodivasapp/src/hooks/RegisterHook.js
+++ portalcodivasapp/src/hooks/RegisterHook.js
@@ -1,26 +1,29 @@
-import { useEffect ,useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import { client } from "../auth/api";
+import  CONSTANTS  from "../constants/index";
 
 const RegisterHook = () => {
-    const [userName ,setUserName] = useState(false)
-    const [userEmail ,setUserEmail] = useState(false)
-    const [userPassword ,setUserPassword] = useState(false)
+  const [userName, setUserName] = useState(false);
+  const [userEmail, setUserEmail] = useState(false);
+  const [userPassword, setUserPassword] = useState(false);
 
-    useEffect( () => {
-        axios.post('https://logf01.herokuapp.com/usuario/cadastro/' , {
-            user_name: 'camis',
-            password: 'senha',
-            email: 'email@email.com'
-        }) .then((response) => response.data) 
-            .then((data) => console.log(data) )
-            .catch((error) => console.log(error.message))
-     } , [] )
-    
-     return {
-         userName,
-         userEmail,
-         userPassword
-     }
-}
+  useEffect(() => {
+    client
+      .post(`${CONSTANTS.API.REGISTRO}`, {
+        user_name: "camis21",
+        password: "senha",
+        email: "email@email43.com.br",
+      })
+      .then((response) => response.data)
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error.message));
+  }, []);
 
-export default RegisterHook
+  return {
+    userName,
+    userEmail,
+    userPassword,
+  };
+};
+
+export default RegisterHook;
