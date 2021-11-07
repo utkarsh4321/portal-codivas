62d9ff9629207f5769e3964517150d2ae43ffa5f
Beatriz Santos <beatrizsantos@MacBook-Pro-de-Beatriz-Santos.local>
2021-11-07 18:23:44 -0300

    feature/cadastro adicionado

--- portalcodivasapp/src/Pages/Register.jsx
+++ portalcodivasapp/src/Pages/Register.jsx
@@ -6,37 +6,38 @@ import RegisterHook from "../hooks/RegisterHook";
 
 function Register() {
   const {
-    userName,
-    userEmail,
-    userPassword
+    handleUserName,
+    handlePassword,
+    handleEmail,
+    handleRegister
 } = RegisterHook()
 
   return (
     <>
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
           <img src="logo.png" width={110} alt="" />
-            <div class="card">
-              <form onsubmit="event.preventDefault()" class="box">
+            <div className="card">
+              <form onSubmit={handleRegister} className="box">
                 <h2>Cadastre - se </h2><br/>
-                <p class="text-muted"> Bem Vinda!</p>
-                <input type="text" name="email" placeholder="Email" />
-                <input type="text" name="username" placeholder="Username" />
-                <input type="password" name="password" placeholder="Password" />
-                {/* <a class="forgot text-muted" href="#">
+                <p className="text-muted"> Bem Vinda!</p>
+                <input type="email" name="email" placeholder="Email" required onChange={handleEmail} />
+                <input type="text" name="username" placeholder="Username" required onChange={handleUserName} />
+                <input type="password" name="password" placeholder="Password" required onChange={handlePassword} />
+                {/* <a className="forgot text-muted" href="#">
                   Esqueceu a senha?
                 </a> */}
-                <input type="submit" name="submit" value="Register" href="#" />
-                <div class="col-md-12">
-                  <ul class="social-network social-circle">
+                <input type="submit" name="submit" value="Register" />
+                <div className="col-md-12">
+                  <ul className="social-network social-circle">
                     <li>
-                      <a href="#" class="icoFacebook" title="Facebook">
+                      <a href="#" className="icoFacebook" title="Facebook">
                         <FontAwesomeIcon icon={["fa", "bell"]} />
                       </a>
                     </li>
                     <li>
-                      <a href="#" class="icoGoogle" title="Google +">
+                      <a href="#" className="icoGoogle" title="Google +">
                       <FontAwesomeIcon icon={["fa", "user-alt"]} />
                       </a>
                     </li>
